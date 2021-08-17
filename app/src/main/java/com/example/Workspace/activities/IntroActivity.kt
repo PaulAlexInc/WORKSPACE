d11812b439088582382deb2a0d6e64d2581113fa
Paul-Alex02012000 <paulblue17@gmail.com>
2021-08-17 10:24:59 +0530

    new repo

---
+++ app/src/main/java/com/example/Workspace/activities/IntroActivity.kt
@@ -0,0 +1,20 @@
+package com.example.Workspace.activities
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.Workspace.R
+import kotlinx.android.synthetic.main.activity_intro.*
+
+class IntroActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+    }
+}
